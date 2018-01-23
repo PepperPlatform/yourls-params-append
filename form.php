99aeb53996152c975cfa8e979f3a9c368e9d1eda
Tinashe <tinashe@pepper.com>
2018-01-23 10:49:04 +0100

    initial commit

---
+++ form.php
@@ -0,0 +1,20 @@
+<main role="main">
+	<div id="new_url">
+		<div>
+			<form id="new_url_form" action="" method="get">
+				<div>
+					<strong><?php yourls_e('Enter the URL'); ?></strong>:<input type="text" id="add-url" name="url" value="" class="text" size="80" placeholder="https://"/>
+					<?php yourls_e('Optional '); ?> :
+					<strong><?php yourls_e('Custom short URL'); ?></strong>:<input type="text" id="add-keyword" name="keyword" value="" class="text" size="8"/>
+					<?php yourls_nonce_field('add_url', 'nonce-add'); ?>
+
+					<br/>
+					<label><strong>Select Platform to append url parameters</strong></label>
+					<?php add_select_to_form() ?>
+					<br/>
+					<input type="button" id="add-button" name="add-button" value="<?php yourls_e('Shorten The URL'); ?>" class="button" onclick="add_link();"/>
+				</div>
+			</form>
+			<div id="feedback" style="display:none"></div>
+		</div>
+	</div>
---
+++ plugin.php
@@ -0,0 +1,141 @@
+<?php
+/*
+Plugin Name: URL Params Append
+Plugin URI: https://github.com/PepperPlatform/yourls-params-append
+Description: Appends url params based on selected platform(e.g facebook is a platform)
+Version: 0.1
+Author: Tinashe - @Pepper
+Author URI: https://github.com/tm57
+*/
+
+// No direct call
+if (!defined('YOURLS_ABSPATH')) {
+	die();
+}
+
+// Add customised form
+yourls_add_filter('shunt_html_addnew', 'add_create_form');
+
+// Show the saved params
+yourls_add_action('show_save_url_params_form', 'show_platform_params');
+
+// Register our plugin
+yourls_register_plugin_page('save_platform_params', 'Save Platform Url Params', 'save_url_params');
+
+function save_url_params() {
+
+	// Check if a form was submitted
+	if (isset($_POST['platform']) && isset($_POST['url_params'])) {
+		// Check nonce
+		yourls_verify_nonce('save_platform_params');
+
+	}
+
+	$platform_name = trim($_POST['platform']);
+	$param = trim($_POST['url_params']);
+
+	db_create_new_params($platform_name, $param);
+
+	// Create nonce
+	$nonce = yourls_create_nonce('save_platform_params');
+
+	echo <<<HTML
+		<h2>Save Platform Url Params</h2>
+		<p>Saves your url params to the database</p>
+		<form method="post">
+		<input type="hidden" name="nonce" value="$nonce" />
+		
+		<p><label for="platform">Enter Platform Name</label> 
+		<input type="text" id="platform" name="platform" value="" />
+		</p>
+		
+		
+		<p><label for="url_params">Enter the url params</label> 
+		<input type="text" id="url_params" name="url_params" value="" style="width: 800px" /></p>
+		
+		<p><input type="submit" value="Save" /></p>
+		</form>
+
+HTML;
+	yourls_do_action('show_save_url_params_form');
+}
+
+function show_platform_params() {
+
+	$data = get_saved_params();
+	$rows = generate_platform_rows($data);
+	echo <<<HTML
+		<h2>Existing Params</h2>
+		
+<table>
+  <tr>
+    <th>Platform</th>
+    <th>Param</th>
+  </tr>
+  $rows
+
+</table>
+HTML;
+}
+
+function render_platform_row($row) {
+
+	return '
+<tr>
+    <td>' . $row->platform_name . '</td>
+    <td>' . $row->params . '</td>
+  </tr>';
+}
+
+function get_saved_params() {
+
+	global $ydb;
+	$table = 'yourls_url_platform_params';
+	$sql = "SELECT * FROM `$table`";
+
+	return $ydb->fetchObjects($sql);
+}
+
+function generate_platform_rows($rows) {
+
+	$result = '';
+	foreach ($rows as $row) {
+		$result .= render_platform_row($row);
+	}
+
+	return $result;
+}
+
+function db_create_new_params($platform, $params) {
+
+	try {
+		global $ydb;
+		$table = 'yourls_url_platform_params';
+		$sql = "INSERT INTO $table (platform_name, params)  VALUES(:platform_name,:params)";
+		$binds = array('platform_name' => $platform, 'params' => $params);
+		$ydb->fetchAffected($sql, $binds);
+	} catch (\Exception $e) {
+		//do nothing lolz
+	}
+}
+
+function add_select_to_form() {
+
+	$existing = get_saved_params();
+
+	$html = '<select name="url_params" id="url_params" form="new_url_form">';
+
+	foreach ($existing as $row) {
+
+		$html .= "<option value='{$row->params}'" . ">$row->platform_name</option>";
+
+	}
+
+	echo $html . '</select>';
+
+}
+
+function add_create_form() {
+
+	include_once 'form.php';
+}
