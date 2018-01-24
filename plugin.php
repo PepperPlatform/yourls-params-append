5e7ca96f80f6a5ec06e35a30b093b4f28c345c48
Tinashe <tinashe@pepper.com>
2018-01-24 13:19:36 +0100

    add ability to delete

--- plugin.php
+++ plugin.php
@@ -28,17 +28,20 @@ yourls_register_plugin_page('save_platform_params', 'Save Platform Url Params', 
 
 function save_url_params() {
 
+
 	// Check if a form was submitted
 	if (isset($_POST['platform']) && isset($_POST['url_params'])) {
 		// Check nonce
 		yourls_verify_nonce('save_platform_params');
 
+
+		$platform_name = trim($_POST['platform']);
+		$param = trim($_POST['url_params']);
+
+		db_create_new_params($platform_name, $param);
+	} elseif (isset($_GET['id'])) {
+		db_remove_platform($_GET['id']);
 	}
-
-	$platform_name = trim($_POST['platform']);
-	$param = trim($_POST['url_params']);
-
-	db_create_new_params($platform_name, $param);
 
 	// Create nonce
 	$nonce = yourls_create_nonce('save_platform_params');
@@ -88,6 +91,7 @@ function render_platform_row($row) {
 <tr>
     <td>' . $row->platform_name . '</td>
     <td>' . $row->params . '</td>
+    <td><a href=' . yourls_admin_url('plugins.php?page=save_platform_params&id=' . $row->platform_id) . '>Delete</a></td>
   </tr>';
 }
 
@@ -123,11 +127,25 @@ function db_create_new_params($platform, $params) {
 	}
 }
 
+function db_remove_platform($id) {
+
+	try {
+		global $ydb;
+		$table = 'yourls_url_platform_params';
+		$sql = "DELETE FROM $table WHERE platform_id = :id";
+		$binds = array('id' => $id);
+		$ydb->fetchAffected($sql, $binds);
+	} catch (\Exception $e) {
+		//do nothing lolz
+	}
+}
+
 function add_select_to_form() {
 
 	$existing = get_saved_params();
 
 	$html = '<select name="url_params" id="url_params" form="new_url_form">';
+	$html .= "<option value=''>Select Platform</option>";
 
 	foreach ($existing as $row) {
 
@@ -157,14 +175,19 @@ function create_link() {
 	var nonce = $("#nonce-add").val();
 	var url_params = $("#url_params").val();
 	var campaign = $("#campaign").val();
+	var params = '';
 	
-	if(!campaign ){
-		var message = 'Please enter the custom campaign';
+	if(url_params && !campaign ){
+		var message = 'Please enter the custom campaign. It is required when platform is selected!';
 		feedback(message, 'error');
 		return;
 		
 	}
-	const params = url_params.trim() + '&utm_campaign='+ campaign.trim();
+	
+	if(url_params){
+	 params = url_params.trim() + '&utm_campaign='+ campaign.trim();
+	}
+	
 	if (!newurl || newurl == 'http://' || newurl == 'https://') {
 		return;
 	}
